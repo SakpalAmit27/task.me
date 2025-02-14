16c5342a8f22a026f16f4f4e4647c0f6322c7810
Sakpal Amit <amitsakpal744@gmail.com>
2025-02-14 19:33:19 +0530

    passed down the props of state (todo , setTodo)  ,as according to the component's behavior

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react"
+
+import TodoList from "./TodoList/TodoList";
+import AddTodo from "./AddTodo/AddTodo";
 
 
 const App = () => {
+
+  const [todo,setTodo] = useState([]);
   return (
     <div>
       task.me
+
+      <TodoList todo={todo} setTodo={setTodo}/> 
+      <AddTodo setTodo={setTodo}/>
+
     </div>
   )
 }
