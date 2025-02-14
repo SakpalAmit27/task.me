7b17c1a736a072f661077ba7ea93324f6d2c702c
Sakpal Amit <amitsakpal744@gmail.com>
2025-02-14 19:51:35 +0530

    implemented the task / setTask state in AddTodo component ,as we need to add task's , and implemented the trim method just in case , it should not be empty

--- src/AddTodo/AddTodo.jsx
+++ src/AddTodo/AddTodo.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react"
 
 
 const AddTodo = ({setTodos}) => {
+
+  const [task,setTask] = useState("");
+
+
+  // function to add the task // 
+
+  const addTask = () => {
+    if(task.trim() === "") return; // this will prevent empty todo // 
+  }
   return (
     <div>
       AddTodo Component
